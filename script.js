d1537ca582dcedd43a74e9a3f639a2d1d0df384e
Vinnie2605 <vincent.voisin26@gmail.com>
2022-11-16 21:31:11 +0100

    test email

--- script.js
+++ script.js
@@ -1737,7 +1737,8 @@ function SendMail(){
     var params={
         from_name : document.getElementById("fullName").value,
         email_id : document.getElementById("email_id").value,
-        message: formationsList.formationsList[0].intitule,
+        message:  document.getElementById("message").value,
+        /** formationsList.formationsList[0].intitule, */
     }
     emailjs.send("service_zfl00os","template_mm1u33n",params).then(function (res) {
         alert("Success"+ res.status);
