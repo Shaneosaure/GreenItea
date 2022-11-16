40e627fb680285309e8bafc22e1ed4d91a9cac0a
Shaneosaure <stephane2306@protonmail.com>
2022-11-16 12:44:36 +0100

    ajout filtre avec retours valeurs

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 /**ficher pour java script */
-var basketList=[];
+var basketList = [];
 
 
 /**liste des formations**/
-let formationsList= {
+let formationsList = {
+}
+
+let filtersList = [];
+
+function filter() {
+    let checkboxes = document.querySelectorAll('input[name="structure"]:checked');
+    let values = [];
+    checkboxes.forEach((checkbox) => {
+        values.push(checkbox.value);
+    });
+    alert(values);
 }
 
 /**fonction d'ajout dans le panier */
-function addBasket(btnId){
+function addBasket(btnId) {
     basketList.push(document.getElementById(btnId));
 }
 /**fonction de retrait du panier */
-function removeBasket(btnId){
+function removeBasket(btnId) {
     for (let n = 0; n < basketList.length; n++) {
-        if(basketList[n] == document.getElementById(btnId)){
+        if (basketList[n] == document.getElementById(btnId)) {
             basketList.splice(n, 1);
         }
     }
 }
 /**fonction pour imprimer la page en pdf */
-function print(){
+function print() {
 
 }
